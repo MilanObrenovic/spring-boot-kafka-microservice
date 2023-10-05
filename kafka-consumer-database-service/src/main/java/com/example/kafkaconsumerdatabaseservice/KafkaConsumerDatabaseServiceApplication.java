37a7bbf0189da5000d3da6ac8689cd1cba3b60b2
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-05 11:44:58 +0200

    Added a kafka consumer module

---
+++ kafka-consumer-database-service/src/main/java/com/example/kafkaconsumerdatabaseservice/KafkaConsumerDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaconsumerdatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerDatabaseServiceApplication.class, args);
+	}
+
+}
